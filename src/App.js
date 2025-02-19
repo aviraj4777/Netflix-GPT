fe94beaceccf74d2bfc9c282388ca23c6cb85d79
aviraj4777 <aviraj4777@gmail.com>
2025-02-19 23:28:57 +0530

    Body Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="App font-bold">
-      Hello World!
-    </div>
+    <Body/>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="absolute px-10 py-5 bg-gradient-to-b from-black">
+      <img className="w-44"
+        src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+        alt="logo"
+      />
+    </div>
+  );
+};
+
+export default Header;
